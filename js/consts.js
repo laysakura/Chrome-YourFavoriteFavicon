478fdc2379352baf644972068a08ae8d2be55e8a
nakatani.sho <nakatani.sho@dena.jp>
2015-03-29 00:04:24 +0900

    interactively shows editing icon

--- js/consts.js
+++ js/consts.js
@@ -1,3 +1,4 @@
 const YFF_ICON_SIZE = 16;
 const YFF_ICON_DATA_URL_FORMAT = 'image_png';
 const YFF_ICON_LINK_TYPE = 'image/png';
+const YFF_UPLOAD_IMG_TYPE_PATTERN = /^image\/(png)$/;
--- js/popup.js
+++ js/popup.js
@@ -4,18 +4,24 @@
   canvas.width = YFF_ICON_SIZE;
   canvas.height = YFF_ICON_SIZE;
 
+  // 初期値はlocalStorageから(modelから)とる
+  var registered_settings = {
+    'bg_color': '#0af',
+  }
 
   //
   // Event listners (Controllers)
+  $('#yff_bg_color').keyup(function() {
+    var bg_color = $(this).val();
+    if (!yff_is_valid_bg_color(bg_color)) return;
+
+    console.log('Background color set:');
+    console.log(bg_color);
+
+    yffCanvasDraw(canvas, bg_color);
+  });
+
   $('#yff_register_btn').click(function() {
-    console.log('click!');
-
-    registered_settings = {
-      'bg_color': $('#yff_bg_color').val(),
-    }
-
-    yffCanvasDraw(canvas, registered_settings.bg_color);
-
     chrome.storage.sync.set(registered_settings, function() {
       console.log('setting has been saved.');
     });
