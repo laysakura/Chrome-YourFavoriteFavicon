743d696361f2cba07cec2ce4d2fb47dd82c34b8c
Sho Nakatani <lay.sakura@gmail.com>
2015-05-03 17:54:55 +0900

    adds simple tests for #matchedUrlPatternsFromBest

--- test/miscs-url.js
+++ test/miscs-url.js
@@ -7,16 +7,26 @@ var yffUrl = new YffUrl();
 
 describe('#matchedUrlPatternsFromBest', function() {
   context('with top-level url', function() {
-    var url = 'http://www.example.com';
+    var url = 'www.example.com';
 
+    // patterns that should match
     data_driven([
-      { patterns: ['http://www.example.com'] },
-      { patterns: ['http://*.example.com'] },
-      { patterns: ['*://www.example.com'] },
-      { patterns: ['*://*.example.com'] },
+      { patterns: ['www.example.com'] },
+      { patterns: ['*.example.com'] },
+      { patterns: ['www.example.com*'] },
+      { patterns: ['*'] },
     ], function() {
       it('should return a matched pattern', function(ctx) {
-        assert.deepEqual([ctx.patterns[0]], yffUrl.matchedUrlPatternsFromBest(ctx.patterns));
+        assert.deepEqual([ctx.patterns[0]], yffUrl.matchedUrlPatternsFromBest(ctx.patterns, url));
+      })
+    })
+
+    // patterns that should not match
+    data_driven([
+      { patterns: ['www.example.jp'] },
+    ], function() {
+      it('should return a matched pattern', function(ctx) {
+        assert.deepEqual([], yffUrl.matchedUrlPatternsFromBest(ctx.patterns, url));
       })
     })
   })
